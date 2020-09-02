d1ce90a08f3912da3f043400fbf189a4f5f35f52
Vikram S <vikram.senthil@outlook.com>
2020-09-02 21:46:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const app= express();
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public"));
-
 mongoose.connect("mongodb://localhost:27017/pmsDB",{useNewUrlParser:true,useUnifiedTopology: true })
 //Users Collection
 //User schema
@@ -381,10 +379,6 @@ app.route("/usergroups/:id")
     }
   )
 });
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
