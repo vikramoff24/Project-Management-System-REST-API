d6ca6abf1822e8385aa330f6c08962fb548133ce
Vikram S <vikram.senthil@outlook.com>
2020-10-03 19:13:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express=require("express");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const ejs=require("ejs");
+
+const app= express();
+
+app.set('view engine',ejs);
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/")
