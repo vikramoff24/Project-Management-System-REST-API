764579ab217b2de9b28059bca7bac6c921479f10
Vikram S <vikram.senthil@outlook.com>
2020-10-03 19:40:09 +0530

    created user schema

--- app.js
+++ app.js
@@ -12,3 +12,25 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/")
+
+//Users schema
+
+const userSchema=
+{
+firstname:String,
+lastname:String,
+password:String,
+mail:String,
+created_at    : { type: Date },
+updated_at    : { type: Date }
+}
+
+userSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+//model
