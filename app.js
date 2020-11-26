78ab70e537061dd29a88576c21ee1ce45e640c7c
Vikram S <vikram.senthil@outlook.com>
2020-11-26 21:48:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const mongoose=require("mongoose");
 const ejs=require("ejs");
 
 const app= express();
-
 app.set('view engine','ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/pmsDB",{useNewUrlParser:true,useUnifiedTopology: true })
